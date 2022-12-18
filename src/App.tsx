070839ef6bc2f4f2b03f51150e5a352149cb6503
John Farrell <jsfarrell12@gmail.com>
2022-12-18 17:15:32 -0500

    cookies

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import * as React from "react";
 import { ChakraProvider, theme } from "@chakra-ui/react";
 import { Form } from "./Components/Form";
+import setVisitorCookie from "./Cookie";
 
+setVisitorCookie("visitorId", 7);
 export const App = () => (
   <ChakraProvider theme={theme}>
     <Form />
---
+++ src/Cookie.ts
@@ -0,0 +1,31 @@
+export default function setVisitorCookie(name: string, expireDays: number) {
+  const visitorIdCookie = getCookie("visitorId=");
+  if (!visitorIdCookie) {
+    createCookie("visitorId", createUniqueId(), 7);
+  } else {
+    console.log(visitorIdCookie);
+  }
+}
+
+function createCookie(name: string, value: string, expireDays: number) {
+  const date = new Date();
+  date.setTime(date.getTime() + expireDays * 24 * 60 * 60 * 1000);
+  const expires = "expires=" + date.toUTCString();
+  document.cookie = name + "=" + value + ";" + expires + ";path=/";
+}
+
+// will return format of: visitorId=51czetjt0lmh2k24gvbobh
+function getCookie(s: string) {
+  const visitorIdCookie = document.cookie
+    .split(";")
+    .find((c: string) => c.trim().startsWith(s));
+  return visitorIdCookie;
+}
+
+// Generate a unique ID for each visitor
+function createUniqueId() {
+  return (
+    Math.random().toString(36).substring(2, 15) +
+    Math.random().toString(36).substring(2, 15)
+  );
+}
