d78533daacfd67b7dce2df2b3f33c21b905ded17
John Farrell <jsfarrell12@gmail.com>
2022-12-17 20:17:53 -0500

    form outline v1

--- src/Components/Form.tsx
+++ src/Components/Form.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import { useState } from "react";
 import {
   Button,
   Center,
@@ -15,29 +15,159 @@ import {
   Select,
 } from "@chakra-ui/react";
 
-// The below import defines which components come from formik
-// import { Field, Form, Formik } from 'formik';
+interface UserCollegeData {
+  [key: string]: {
+    [key: string]: any;
+  };
+  academic: {
+    gpa: number;
+    sat: number;
+    act: number;
+  };
+  courseload: {
+    honors: number;
+    apib: number;
+    lang: number;
+    cs: number;
+    core: number;
+    major: number;
+  };
+  confidence: {
+    extracurriculars: number;
+    essay: number;
+    rewards: number;
+    recommendations: number;
+    volunteering: number;
+    work: number;
+    talent: number;
+    interviewing: number;
+    character: number;
+    interest: number;
+  };
+  colleges: {
+    legacy: string[];
+    alumni: string[];
+    feeder: string[];
+  };
+  residency: {
+    zipcode: number;
+    state: string;
+    country: string;
+  };
+  class: {
+    size: number;
+    rank: number;
+  };
+  adversity: {
+    fgen: boolean;
+    international: boolean;
+    transfer: boolean;
+  };
+}
 
 export function Form() {
+  const [formData, setFormData] = useState<UserCollegeData>({
+    academic: {
+      gpa: -1,
+      sat: -1,
+      act: -1,
+    },
+    courseload: {
+      honors: -1,
+      apib: -1,
+      lang: -1,
+      cs: -1,
+      core: -1,
+      major: -1,
+    },
+    confidence: {
+      extracurriculars: -1,
+      essay: -1,
+      rewards: -1,
+      recommendations: -1,
+      volunteering: -1,
+      work: -1,
+      talent: -1,
+      interviewing: -1,
+      character: -1,
+      interest: -1,
+    },
+    colleges: {
+      legacy: [],
+      alumni: [],
+      feeder: [],
+    },
+    residency: {
+      zipcode: -1,
+      state: "",
+      country: "",
+    },
+    class: {
+      size: -1,
+      rank: -1,
+    },
+    adversity: {
+      fgen: false,
+      international: false,
+      transfer: false,
+    },
+  });
+
+  function handleChange(event: any) {
+    console.log(formData);
+    const { name, value } = event.target;
+    const [parent, child] = name.split(".");
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [parent]: {
+        ...prevFormData[parent],
+        [child]: value,
+      },
+    }));
+  }
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    console.log("submit");
+    event.preventDefault();
+    console.log(formData);
+    // Make an API call to the endpoint with the form data
+    // setFormData({ name: '', email: '', message: '' });
+  };
+
   return (
     <Center>
       <Container>
-        <form>
+        <form onSubmit={handleSubmit}>
           <Heading as="h4" size="md">
             Testing and GPA
           </Heading>
           <Grid templateColumns="repeat(3, 1fr)" gap={1}>
             <InputGroup>
               <InputLeftAddon children="SAT" />
-              <Input type="text" placeholder="Out of 1600" />
+              <Input
+                onChange={handleChange}
+                name={"academic.sat"}
+                type="text"
+                placeholder="Out of 1600"
+              />
             </InputGroup>
             <InputGroup>
               <InputLeftAddon children="ACT" />
-              <Input type="text" placeholder="Out of 36" />
+              <Input
+                onChange={handleChange}
+                name={"academic.act"}
+                type="text"
+                placeholder="Out of 36"
+              />
             </InputGroup>
             <InputGroup>
               <InputLeftAddon children="GPA" />
-              <Input type="text" placeholder="Out of 4.00" />
+              <Input
+                onChange={handleChange}
+                name={"academic.gpa"}
+                type="text"
+                placeholder="Out of 4.00"
+              />
             </InputGroup>
           </Grid>
           <Heading as="h4" size="md">
@@ -45,7 +175,11 @@ export function Form() {
           </Heading>
           <Grid templateColumns="repeat(3, 1fr)" gap={1}>
             <InputGroup>
-              <Select placeholder="Honors Courses">
+              <Select
+                onChange={handleChange}
+                name={"courseload.honors"}
+                placeholder="Honors Courses"
+              >
                 <option value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
@@ -58,7 +192,11 @@ export function Form() {
               </Select>
             </InputGroup>
             <InputGroup>
-              <Select placeholder="AP/IB Courses">
+              <Select
+                onChange={handleChange}
+                name={"courseload.apib"}
+                placeholder="AP/IB Courses"
+              >
                 <option value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
@@ -71,7 +209,11 @@ export function Form() {
               </Select>
             </InputGroup>
             <InputGroup>
-              <Select placeholder="Lang Courses">
+              <Select
+                onChange={handleChange}
+                name={"courseload.lang"}
+                placeholder="Lang Courses"
+              >
                 <option value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
@@ -84,7 +226,11 @@ export function Form() {
               </Select>
             </InputGroup>
             <InputGroup>
-              <Select placeholder="CS Courses">
+              <Select
+                onChange={handleChange}
+                name={"courseload.cs"}
+                placeholder="CS Courses"
+              >
                 <option value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
@@ -97,7 +243,11 @@ export function Form() {
               </Select>
             </InputGroup>
             <InputGroup>
-              <Select placeholder="Core Courses">
+              <Select
+                onChange={handleChange}
+                name={"courseload.core"}
+                placeholder="Core Courses"
+              >
                 <option value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
@@ -110,7 +260,11 @@ export function Form() {
               </Select>
             </InputGroup>
             <InputGroup>
-              <Select placeholder="Major Related">
+              <Select
+                onChange={handleChange}
+                name={"courseload.major"}
+                placeholder="Major Related"
+              >
                 <option value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
@@ -127,7 +281,10 @@ export function Form() {
             Confidence (Low - 1, High - 5)
           </Heading>
           <Grid>
-            <RadioGroup>
+            <RadioGroup
+              onChange={handleChange}
+              name={"confidence.extracurriculars"}
+            >
               <Heading as="h3" size="sm">
                 Extracurriculars
               </Heading>
@@ -139,7 +296,7 @@ export function Form() {
                 <Radio value="5">5</Radio>
               </Flex>
             </RadioGroup>
-            <RadioGroup>
+            <RadioGroup onChange={handleChange} name={"confidence.essay"}>
               <Heading as="h3" size="sm">
                 Essay
               </Heading>
@@ -151,7 +308,7 @@ export function Form() {
                 <Radio value="5">5</Radio>
               </Flex>
             </RadioGroup>
-            <RadioGroup>
+            <RadioGroup onChange={handleChange} name={"confidence.awards"}>
               <Heading as="h3" size="sm">
                 Awards
               </Heading>
@@ -163,7 +320,10 @@ export function Form() {
                 <Radio value="5">5</Radio>
               </Flex>
             </RadioGroup>
-            <RadioGroup>
+            <RadioGroup
+              onChange={handleChange}
+              name={"confidence.recommendations"}
+            >
               <Heading as="h3" size="sm">
                 Recommendations
               </Heading>
@@ -175,7 +335,10 @@ export function Form() {
                 <Radio value="5">5</Radio>
               </Flex>
             </RadioGroup>
-            <RadioGroup>
+            <RadioGroup
+              onChange={handleChange}
+              name={"confidence.volunteering"}
+            >
               <Heading as="h3" size="sm">
                 Volunteering
               </Heading>
@@ -187,7 +350,7 @@ export function Form() {
                 <Radio value="5">5</Radio>
               </Flex>
             </RadioGroup>
-            <RadioGroup>
+            <RadioGroup onChange={handleChange} name={"confidence.work"}>
               <Heading as="h3" size="sm">
                 Work
               </Heading>
@@ -199,7 +362,7 @@ export function Form() {
                 <Radio value="5">5</Radio>
               </Flex>
             </RadioGroup>
-            <RadioGroup>
+            <RadioGroup onChange={handleChange} name={"confidence.talent"}>
               <Heading as="h3" size="sm">
                 Talent
               </Heading>
@@ -211,7 +374,10 @@ export function Form() {
                 <Radio value="5">5</Radio>
               </Flex>
             </RadioGroup>
-            <RadioGroup>
+            <RadioGroup
+              onChange={handleChange}
+              name={"confidence.interviewing"}
+            >
               <Heading as="h3" size="sm">
                 Interviewing
               </Heading>
@@ -223,7 +389,7 @@ export function Form() {
                 <Radio value="5">5</Radio>
               </Flex>
             </RadioGroup>
-            <RadioGroup>
+            <RadioGroup onChange={handleChange} name={"confidence.character"}>
               <Heading as="h3" size="sm">
                 Character
               </Heading>
@@ -235,7 +401,7 @@ export function Form() {
                 <Radio value="5">5</Radio>
               </Flex>
             </RadioGroup>
-            <RadioGroup>
+            <RadioGroup onChange={handleChange} name={"confidence.interest"}>
               <Heading as="h3" size="sm">
                 Interest
               </Heading>
@@ -257,15 +423,30 @@ export function Form() {
           <Grid templateColumns="repeat(3, 1fr)" gap={1}>
             <InputGroup>
               <InputLeftAddon children="Mom" />
-              <Input type="text" placeholder="University" />
+              <Input
+                onChange={handleChange}
+                name={"colleges.legacy"}
+                type="text"
+                placeholder="University"
+              />
             </InputGroup>
             <InputGroup>
               <InputLeftAddon children="Dad" />
-              <Input type="text" placeholder="University" />
+              <Input
+                onChange={handleChange}
+                name={"colleges.legacy"}
+                type="text"
+                placeholder="University"
+              />
             </InputGroup>
             <InputGroup>
               <InputLeftAddon children="Other" />
-              <Input type="text" placeholder="University" />
+              <Input
+                onChange={handleChange}
+                name={"colleges.legacy"}
+                type="text"
+                placeholder="University"
+              />
             </InputGroup>
           </Grid>
           <Heading as="h3" size="sm">
@@ -274,15 +455,30 @@ export function Form() {
           <Grid templateColumns="repeat(3, 1fr)" gap={1}>
             <InputGroup>
               <InputLeftAddon children="#1" />
-              <Input type="text" placeholder="University" />
+              <Input
+                onChange={handleChange}
+                name={"colleges.alumni"}
+                type="text"
+                placeholder="University"
+              />
             </InputGroup>
             <InputGroup>
               <InputLeftAddon children="#2" />
-              <Input type="text" placeholder="University" />
+              <Input
+                onChange={handleChange}
+                name={"colleges.alumni"}
+                type="text"
+                placeholder="University"
+              />
             </InputGroup>
             <InputGroup>
               <InputLeftAddon children="#3" />
-              <Input type="text" placeholder="University" />
+              <Input
+                onChange={handleChange}
+                name={"colleges.alumni"}
+                type="text"
+                placeholder="University"
+              />
             </InputGroup>
           </Grid>
           <Heading as="h3" size="sm">
@@ -291,32 +487,30 @@ export function Form() {
           <Grid templateColumns="repeat(3, 1fr)" gap={1}>
             <InputGroup>
               <InputLeftAddon children="#1" />
-              <Input type="text" placeholder="University" />
+              <Input
+                onChange={handleChange}
+                name={"colleges.feeder"}
+                type="text"
+                placeholder="University"
+              />
             </InputGroup>
             <InputGroup>
               <InputLeftAddon children="#2" />
-              <Input type="text" placeholder="University" />
+              <Input
+                onChange={handleChange}
+                name={"colleges.feeder"}
+                type="text"
+                placeholder="University"
+              />
             </InputGroup>
             <InputGroup>
               <InputLeftAddon children="#3" />
-              <Input type="text" placeholder="University" />
-            </InputGroup>
-          </Grid>
-          <Heading as="h3" size="sm">
-            Feeder
-          </Heading>
-          <Grid templateColumns="repeat(3, 1fr)" gap={1}>
-            <InputGroup>
-              <InputLeftAddon children="#1" />
-              <Input type="text" placeholder="University" />
-            </InputGroup>
-            <InputGroup>
-              <InputLeftAddon children="#2" />
-              <Input type="text" placeholder="University" />
-            </InputGroup>
-            <InputGroup>
-              <InputLeftAddon children="#3" />
-              <Input type="text" placeholder="University" />
+              <Input
+                onChange={handleChange}
+                name={"colleges.feeder"}
+                type="text"
+                placeholder="University"
+              />
             </InputGroup>
           </Grid>
           <Heading as="h3" size="sm">
@@ -325,17 +519,30 @@ export function Form() {
           <Grid templateColumns="repeat(3, 1fr)" gap={1}>
             <InputGroup>
               <InputLeftAddon children="ZIP" />
-              <Input type="text" placeholder="14450" />
+              <Input
+                onChange={handleChange}
+                name={"residency.zipcode"}
+                type="text"
+                placeholder="14450"
+              />
             </InputGroup>
             <InputGroup>
-              <Select placeholder="State">
+              <Select
+                onChange={handleChange}
+                name={"residency.state"}
+                placeholder="State"
+              >
                 <option value="NY">New York</option>
                 <option value="WI">Wisconson</option>
                 <option value="CA">California</option>
               </Select>
             </InputGroup>
             <InputGroup>
-              <Select placeholder="Country">
+              <Select
+                onChange={handleChange}
+                name={"residency.country"}
+                placeholder="Country"
+              >
                 <option value="US">United States</option>
               </Select>
             </InputGroup>
@@ -346,11 +553,21 @@ export function Form() {
           <Grid templateColumns="repeat(2, 1fr)" gap={1}>
             <InputGroup>
               <InputLeftAddon children="Class Rank" />
-              <Input type="text" placeholder="12" />
+              <Input
+                onChange={handleChange}
+                name={"class.rank"}
+                type="text"
+                placeholder="12"
+              />
             </InputGroup>
             <InputGroup>
               <InputLeftAddon children="Class Size" />
-              <Input type="text" placeholder="244" />
+              <Input
+                onChange={handleChange}
+                name={"class.size"}
+                type="text"
+                placeholder="244"
+              />
             </InputGroup>
           </Grid>
           <Heading as="h3" size="sm">
@@ -358,16 +575,27 @@ export function Form() {
           </Heading>
           <Grid templateColumns="repeat(3, 1fr)" gap={1}>
             <InputGroup>
-              <Checkbox>First Gen</Checkbox>
+              <Checkbox onChange={handleChange} name={"adversity.fgen"}>
+                First Gen
+              </Checkbox>
             </InputGroup>
             <InputGroup>
-              <Checkbox>International</Checkbox>
+              <Checkbox
+                onChange={handleChange}
+                name={"adversity.international"}
+              >
+                International
+              </Checkbox>
             </InputGroup>
             <InputGroup>
-              <Checkbox>Transfer</Checkbox>
+              <Checkbox onChange={handleChange} name={"adversity.transfer"}>
+                Transfer
+              </Checkbox>
             </InputGroup>
           </Grid>
-          <Button colorScheme="teal">Submit</Button>
+          <Button type={"submit"} colorScheme="teal">
+            Submit
+          </Button>
         </form>
       </Container>
     </Center>
