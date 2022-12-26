5b7ab3facd7e66dd710df577edd6988ff70d2da8
John Farrell <jsfarrell12@gmail.com>
2022-12-26 13:25:28 -0500

    rem

--- src/Components/SignIn.tsx
+++ src/Components/SignIn.tsx
@@ -10,10 +10,7 @@ export function SignIn() {
 
   function handleGoogleLoginSuccess(tokenResponse: any) {
     const accessToken = tokenResponse.access_token;
-    dispatch(continueGoogle(accessToken, navigate)).then(() => {
-      window.location.assign("https://college-list-maker.github.io/client/#/");
-      window.location.reload();
-    });
+    dispatch(continueGoogle(accessToken, navigate));
   }
   function handleGoogleError(err: any) {
     console.log(err);
