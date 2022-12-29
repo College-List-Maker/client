ddeea00c951b50239e821e78ff399a76d17bf572
Tyler Cady <tylercadyfairport@gmail.com>
2022-12-28 19:37:55 -0500

    Oopsie

--- src/Components/Form.tsx
+++ src/Components/Form.tsx
@@ -21,125 +21,125 @@ import { getCookie } from "../Cookie";
 import { UserCollegeData } from "../types";
 
 export function Form() {
-  const defaultData: UserCollegeData = {
-    academic: {
-      gpa: -1,
-      sat: -1,
-      act: -1,
-    },
-    courseload: {
-      honors: -1,
-      apib: -1,
-      lang: -1,
-      cs: "",
-      core: "",
-      major: -1,
-    },
-    confidence: {
-      extracurriculars: -1,
-      essay: -1,
-      awards: -1,
-      recommendations: -1,
-      volunteering: -1,
-      works: -1,
-      talents: -1,
-      interviewing: -1,
-      character: -1,
-      interest: -1,
-    },
-    colleges: {
-      legacy1: "",
-      legacy2: "",
-      legacy3: "",
-      alumni1: "",
-      alumni2: "",
-      alumni3: "",
-      feeder1: "",
-      feeder2: "",
-      feeder3: "",
-    },
-    residency: {
-      zipcode: -1,
-      state: "",
-      country: "",
-    },
-    class: {
-      size: -1,
-      rank: -1,
-    },
-    adversity: {
-      fgen: false,
-      international: false,
-      transfer: false,
-    },
-    collegePrefs: {
-      coedImportance: -1,
-      academicResourcesImportance: -1,
-      facilityImportance: false,
-      gender: "",
-      hbcuImportance: -1,
-      internshipImportance: -1,
-      majorProminenceImportance: false,
-      pref4yr: false,
-      prefCommittedFaculty: -1,
-      prefHighestDegree: -1,
-      prefMajor: "",
-      prefPrivateControl: false,
-      prefPublicControl: false,
-      prefReligion: -1,
-      prefReligious: false,
-      prefSexRatioF: -1,
-      prefSize: -1,
-      prestiegeImportance: -1,
-      researchImportance: -1,
-      rigorImportance: -1,
-      sameGenderImportance: -1,
-      studyAbroadImportance: -1,
-      workStudyImportance: -1,
-    },
-    costPrefs: {
-      costImportance: false,
-      federalAidImportance: false,
-      income: -1,
-      prefCOA: -1,
-    },
-    locationPrefs: {
-      locationImportance: false,
-      ZIP: -1,
-      curState: "",
-      prefCity: "",
-      prefState: "",
-      prefRegion: -1,
-      livingAtHome: false,
-      prefLocale: -1,
-      prefSummerClimate: -1,
-      prefWinterClimate: -1,
-    },
-    successPrefs: {
-      successImportance: false,
-      alumniCarreerImportance: false,
-      desiredEarnings: -1,
-      graduationRateImportance: false,
-      prefGraduationRate: -1,
-      prefRetentionRate: -1,
-      retentionRateImportance: false,
-    },
-    weights: {
-      collegeWeight: -1,
-      costWeight: -1,
-      locationWeight: -1,
-      successWeight: -1,
-    },
-    listLengths: {
-      reaches: -1,
-      safeties: -1,
-      targets: -1,
-    },
-  };
+    const defaultData: UserCollegeData = {
+        academic: {
+            gpa: -1,
+            sat: -1,
+            act: -1,
+        },
+        courseload: {
+            honors: -1,
+            apib: -1,
+            lang: -1,
+            cs: "",
+            core: "",
+            major: -1,
+        },
+        confidence: {
+            extracurriculars: -1,
+            essay: -1,
+            awards: -1,
+            recommendations: -1,
+            volunteering: -1,
+            works: -1,
+            talents: -1,
+            interviewing: -1,
+            character: -1,
+            interest: -1,
+        },
+        colleges: {
+            legacy1: "",
+            legacy2: "",
+            legacy3: "",
+            alumni1: "",
+            alumni2: "",
+            alumni3: "",
+            feeder1: "",
+            feeder2: "",
+            feeder3: "",
+        },
+        residency: {
+            zipcode: -1,
+            state: "",
+            country: "",
+        },
+        class: {
+            size: -1,
+            rank: -1,
+        },
+        adversity: {
+            fgen: false,
+            international: false,
+            transfer: false,
+        },
+        collegePrefs: {
+            coedImportance: -1,
+            academicResourcesImportance: -1,
+            facilityImportance: false,
+            gender: "",
+            hbcuImportance: -1,
+            internshipImportance: -1,
+            majorProminenceImportance: false,
+            pref4yr: false,
+            prefCommittedFaculty: -1,
+            prefHighestDegree: -1,
+            prefMajor: "",
+            prefPrivateControl: false,
+            prefPublicControl: false,
+            prefReligion: -1,
+            prefReligious: false,
+            prefSexRatioF: -1,
+            prefSize: -1,
+            prestiegeImportance: -1,
+            researchImportance: -1,
+            rigorImportance: -1,
+            sameGenderImportance: -1,
+            studyAbroadImportance: -1,
+            workStudyImportance: -1,
+        },
+        costPrefs: {
+            costImportance: false,
+            federalAidImportance: false,
+            income: -1,
+            prefCOA: -1,
+        },
+        locationPrefs: {
+            locationImportance: false,
+            ZIP: -1,
+            curState: "",
+            prefCity: "",
+            prefState: "",
+            prefRegion: -1,
+            livingAtHome: false,
+            prefLocale: -1,
+            prefSummerClimate: -1,
+            prefWinterClimate: -1,
+        },
+        successPrefs: {
+            successImportance: false,
+            alumniCarreerImportance: false,
+            desiredEarnings: -1,
+            graduationRateImportance: false,
+            prefGraduationRate: -1,
+            prefRetentionRate: -1,
+            retentionRateImportance: false,
+        },
+        weights: {
+            collegeWeight: -1,
+            costWeight: -1,
+            locationWeight: -1,
+            successWeight: -1,
+        },
+        listLengths: {
+            reaches: -1,
+            safeties: -1,
+            targets: -1,
+        },
+    };
 
-  const [formData, setFormData] = useState<UserCollegeData>(defaultData);
+    const [formData, setFormData] = useState<UserCollegeData>(defaultData);
 
-  const toast = useToast();
+    const toast = useToast();
 
     function toggleFirstGenValue() {
         setFormData((prevFormData) => ({
@@ -324,59 +324,55 @@ export function Form() {
         }));
     }
 
-  function handleRadioClickCollege(name: string, value: number) {
-    setFormData((prevFormData) => ({
-      ...prevFormData,
-      collegePrefs: {
-        ...prevFormData["collegePrefs"],
-        [name]: value,
-      },
-    }));
-  }
+    function handleRadioClickCollege(name: string, value: number) {
+        setFormData((prevFormData) => ({
+            ...prevFormData,
+            collegePrefs: {
+                ...prevFormData["collegePrefs"],
+                [name]: value,
+            },
+        }));
+    }
 
-  function handleChange(event: any) {
-    const { name, value } = event.target;
-    const [parent, child] = name.split(".");
-    setFormData((prevFormData) => ({
-      ...prevFormData,
-      [parent]: {
-        ...prevFormData[parent],
-        [child]: value,
-      },
-    }));
-  }
+    function handleChange(event: any) {
+        const { name, value } = event.target;
+        const [parent, child] = name.split(".");
+        setFormData((prevFormData) => ({
+            ...prevFormData,
+            [parent]: {
+                ...prevFormData[parent],
+                [child]: value,
+            },
+        }));
+    }
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    axios
-      .post(
-        "https://collegy-server.herokuapp.com/college/submit-data/" +
-          getCookie("visitorId="),
-        formData
-      )
-      .then((res: any) => {
-        toast({
-          title: "Details submitted.",
-          description: "Your colleges will appear soon.",
-          status: "success",
-          duration: 5000,
-          isClosable: true,
-        });
-      })
-      .catch((err: any) => {
-        console.error(err);
-        toast({
-          title: "Error",
-          description: "Please try again.",
-          status: "error",
-          duration: 5000,
-          isClosable: true,
-        });
-      });
-
-    window.location.hash = "#college-list";
-  };
-
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        axios
+            .post(
+                "https://collegy-server.herokuapp.com/college/submit-data/" +
+                    getCookie("visitorId="),
+                formData
+            )
+            .then((res: any) => {
+                toast({
+                    title: "Details submitted.",
+                    description: "Your colleges will appear soon.",
+                    status: "success",
+                    duration: 5000,
+                    isClosable: true,
+                });
+            })
+            .catch((err: any) => {
+                console.error(err);
+                toast({
+                    title: "Error",
+                    description: "Please try again.",
+                    status: "error",
+                    duration: 5000,
+                    isClosable: true,
+                });
+            });
         window.location.hash = "#college-list";
     };
 
