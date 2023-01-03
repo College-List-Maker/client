0b2a70d7b2f1c80e1f5b6041717844d4d58d8ea7
Tyler Cady <tylercadyfairport@gmail.com>
2023-01-02 22:06:04 -0500

    unitid addition

--- src/api/index.js
+++ src/api/index.js
@@ -1,20 +1,20 @@
 import axios from "axios";
 import { getCookie } from "../Fetch";
 
-const API = axios.create({ baseURL: "https://collegy-server.herokuapp.com" });
+const API = axios.create({ baseURL: "http://localhost:4000" });
 
 API.interceptors.request.use((req) => {
-  if (localStorage.getItem("user_info")) {
-    req.headers.Authorization = `Bearer ${JSON.parse(
-      localStorage.getItem("user_info").token
-    )}`;
-  }
+    if (localStorage.getItem("user_info")) {
+        req.headers.Authorization = `Bearer ${JSON.parse(
+            localStorage.getItem("user_info").token
+        )}`;
+    }
 
-  return req;
+    return req;
 });
 
 export const continueGoogle = (accessToken) =>
-  API.post("/auth/continue", {
-    googleAccessToken: accessToken,
-    visitorId: getCookie("visitorId="),
-  });
+    API.post("/auth/continue", {
+        googleAccessToken: accessToken,
+        visitorId: getCookie("visitorId="),
+    });
